af6864d409bdacde546442893ce54859fac91208
Philip Jorgensen <pjorgensen@lenovo.com>
2023-12-13 13:55:26 -0500

    Update Remediate-SystemUpdateLogging.ps1
    Add Force switch parameter in case reg key doesn't exist

--- Remediations/Apps/Commercial Vantage/Remediate-SystemUpdateLogging.ps1
+++ Remediations/Apps/Commercial Vantage/Remediate-SystemUpdateLogging.ps1
@@ -1,6 +1,6 @@
 Write-Output -InputObject "Enabling System Update Logging..."
 $Path = "HKLM:\SOFTWARE\WOW6432Node\Lenovo\SystemUpdateAddin\Logs"
 If (-not(Test-Path -Path $Path)) {
-    New-Item -Path $Path
+    New-Item -Path $Path -Force
 }
 Set-ItemProperty $Path EnableLogs -Value $true
