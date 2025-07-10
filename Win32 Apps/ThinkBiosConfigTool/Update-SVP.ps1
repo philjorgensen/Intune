0f7cd8e33593cd5a2365c736aad3b34e4626827f
Philip Jorgensen <pjorgensen@lenovo.com>
2025-07-10 09:28:59 -0400

    Update Update-SVP.ps1
    Changed Start-Process to use mshta.exe to resolve an issue where parameters were dropped when using cmd.exe

--- Win32 Apps/ThinkBiosConfigTool/Update-SVP.ps1
+++ Win32 Apps/ThinkBiosConfigTool/Update-SVP.ps1
@@ -1,53 +1,79 @@
+<#
+    .SYNOPSIS
+    Think BIOS Config Tool Supervisor Password Update Script
+
+    .DESCRIPTION
+    This script updates the Supervisor Password (SVP) using the Think BIOS Config Tool (TBCT). This tool does not support setting an initial SVP, so it is intended to be run on devices that already have an SVP set.
+    The script checks for the existence of the TBCT HTA file and a password file, creates necessary directories, and executes the TBCT to change the SVP. It also checks the log file for success and sets a status file for Intune detection.
+
+    .NOTES
+    Version: 1.0    - Initial release - 2024-07-15
+    Version: 2.0    - Changed Start-Process to use ms 
+
+#>
+
 # Define variables
 $lenovoPath = Join-Path -Path $env:ProgramData -ChildPath "Lenovo\ThinkBiosConfig"
 $htaFilePath = Join-Path -Path $PSScriptRoot -ChildPath "ThinkBiosConfig.hta"
 $passFilePath = (Get-ChildItem -Path $PSScriptRoot -Filter "*.ini").FullName
-$secretKey = ""
-$arguments = "`"file=$passFilePath`" `"pass=$secretKey`" `"log=$lenovoPath`""
+$secretKey = "" # Set secret key or password here
+$arguments = "`"`" `"file=$passFilePath`" `"pass=$secretKey`" `"log=$lenovoPath`""
 $statusFilePath = Join-Path -Path $lenovoPath -ChildPath "svp.status"
 
-try {
+try
+{
     # Check if ThinkBiosConfig.hta and password file exist
     $htaExists = Test-Path -Path $htaFilePath -PathType Leaf
     $passFileExists = Test-Path -Path $passFilePath -PathType Leaf
 
-    if ($htaExists -and $passFileExists) {
+    if ($htaExists -and $passFileExists)
+    {
         # Remove previous log file if it exists
         Get-ChildItem -Path $lenovoPath -Filter "*.txt" -ErrorAction SilentlyContinue | Remove-Item -Force
 
         # Check if Lenovo path exists, if not, create it
-        if (-not (Test-Path -Path $lenovoPath -PathType Container)) {
+        if (-not (Test-Path -Path $lenovoPath -PathType Container))
+        {
             Write-Output "Creating Think Bios Config Tool directory"
             New-Item -ItemType Directory -Path $lenovoPath -Force -ErrorAction Stop | Out-Null
         }
 
         # Change SVP using TBCT
         Write-Output "Changing Supervisor Password using Think Bios Config Tool"
-        Start-Process -FilePath $env:SystemRoot\system32\cmd.exe -ArgumentList "/c $htaFilePath $arguments" -Wait -NoNewWindow
+        $msHtaPath = "$($env:SystemRoot)\System32\mshta.exe"
+        Start-Process -FilePath $msHtaPath -ArgumentList "$htaFilePath $arguments" -Wait -NoNewWindow
 
         # Grab status from log file to determine if a reboot is required
         $logFile = (Get-ChildItem -Path $lenovoPath -Filter "*.txt").FullName
-        if ($logFile) {
+        if ($logFile)
+        {
             # Read the content of the log file
             $logContent = Get-Content -Path $logFile
 
             # Check for "Success" in the log content
-            if ($logContent | Select-String -Pattern "Success") {
+            if ($logContent | Select-String -Pattern "Success")
+            {
                 Write-Output "Supervisor Password changed successfully"
                 # Write a status file for Intune detection
                 Set-Content -Path $statusFilePath -Value "SVP changed" -Force
                 # Flag to tell Intune a Soft Reboot is needed in order to update the SVP
-                Exit 3010
-            } else {
+                exit 3010
+            }
+            else
+            {
                 # Cannot set an initial SVP
-                Exit 1
+                exit 1
             }
         }
-    } else {
-        # ThinkBiosConfig.hta or password file not found.
-        Exit 1
     }
-} catch {
+    else
+    {
+        # ThinkBiosConfig.hta or password file not found.
+        exit 1
+    }
+}
+catch
+{
     Write-Error "An error occurred: $_"
-    Exit 1
+    exit 1
 }
